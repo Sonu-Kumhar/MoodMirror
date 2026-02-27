19e38a933667254863e90d37db9187cbf916e816
Sonu Kumhar <kumharsonu899@gmail.com>
2026-02-27 11:58:53 +0530

    feat: add authentication

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,7 +1,11 @@
 const {Router} = require("express")
+const authController = require("../controllers/auth.controller")
 
 const router = Router()
 
 
 
-// router.post("/register", )
+router.post("/register", authController.registerUser)
+router.post("/login", authController.loginUser)
+
+module.exports = router
