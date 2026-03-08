b6840859e79f9521973d10cbe3ddc9035b36a2b5
Sonu Kumhar <kumharsonu899@gmail.com>
2026-03-08 12:18:09 +0530

    style: add style to login page

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import "../style/login.scss"
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <main className="login-page">
+      <div className="form-container">
+        <h1>Login</h1>
+        <form>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input type="text" id='email' name='email' required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="text" id='password' name='password' required />
+          </div>
+          <button className='button' type='submit'>Login</button>
+        </form>
+      </div>
+    </main>
   )
 }
 
