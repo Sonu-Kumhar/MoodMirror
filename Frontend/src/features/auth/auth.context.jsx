e3b5c64235462a7ccc9203c3cc926607659de5df
Sonu Kumhar <kumharsonu899@gmail.com>
2026-03-09 11:34:33 +0530

    add state layer

---
+++ Frontend/src/features/auth/auth.context.jsx
@@ -0,0 +1,20 @@
+//State layer -> to store the user data, loading state
+
+import { createContext } from "react";
+
+export const AuthContext = createContext()
+
+
+
+export const AuthProvider = ({children}) => {
+
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    return (
+        <AuthContext.Provider value={{user, setUser, loading, setLoading}}>
+            {children}
+        </AuthContext.Provider>
+    )
+
+}
