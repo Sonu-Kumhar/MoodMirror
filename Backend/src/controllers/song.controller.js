56aaefa9fe49c54bc8d84e805225282252098cf7
Sonu Kumhar <kumharsonu899@gmail.com>
2026-03-13 19:58:23 +0530

    use node-id3 package to read song file data

--- Backend/src/controllers/song.controller.js
+++ Backend/src/controllers/song.controller.js
@@ -1,11 +1,17 @@
 const songModel = require("../models/song.model")
+const id3 = require("node-id3")
 
 /**
- * 
+ *
  */
 
 async function uploadSong(req, res) {
-    console.log(req.file)
+    const songBuffer = req.file.buffer
+    // console.log(req.file)
+    const tags = id3.read(songBuffer) //to extract the details of song file
+
+    console.log(tags)
+    
 }
 
 
