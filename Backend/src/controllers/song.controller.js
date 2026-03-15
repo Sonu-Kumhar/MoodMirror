8ab25833b104af613a0cc4b4a28209455b78fd73
Sonu Kumhar <kumharsonu899@gmail.com>
2026-03-15 10:04:37 +0530

    optimize song upload time with promise.all

--- Backend/src/controllers/song.controller.js
+++ Backend/src/controllers/song.controller.js
@@ -8,21 +8,23 @@ const storageService = require("../services/storage.service")
 
 async function uploadSong(req, res) {
     const songBuffer = req.file.buffer
-    const {mood} = req.body
+    const { mood } = req.body
     // console.log(req.file)
     const tags = id3.read(songBuffer) //to extract the details of song file
 
-    const songFile = await storageService.uploadFile({
-        buffer: songBuffer,
-        filename: tags.title + ".mp3",
-        folder: "/moodmirror/songs"
-    })
+    const [songFile, posterFile] = await Promise.all([
+        storageService.uploadFile({
+            buffer: songBuffer,
+            filename: tags.title + ".mp3",
+            folder: "/cohort-2/moodify/songs"
+        }),
+        storageService.uploadFile({
+            buffer: tags.image.imageBuffer,
+            filename: tags.title + ".jpeg",
+            folder: "/cohort-2/moodify/posters"
+        })
+    ])
 
-    const posterFile = await storageService.uploadFile({
-        buffer: tags.image.imageBuffer,
-        filename: tags.title + ".jpeg",
-        folder:"/moodmirror/posters"
-    })
 
     const song = await songModel.create({
         title: tags.title,
